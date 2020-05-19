399e8c088e018f62c10825d246b447330b615e90
Travis Vachon <travis.vachon@gmail.com>
2020-05-18 23:13:10 -0700

    new font, fix button link styling

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -5,5 +5,5 @@ import Button from '@material-ui/core/Button';
 import Link from "../components/Link"
 
 export default (props) => (
-  <Button component={Link} {...props} />
+  <Button component={Link} style={{color: 'inherit', textDecoration: 'inherit'}} {...props} />
 )
--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const darkTheme = createMuiTheme({
   palette: {
     type: "dark"
+  },
+  typography: {
+    fontFamily:['gryphius-mvb']
   }
 })
 
