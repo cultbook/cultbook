f3d034321289026a9a2d2a6747febd35758a5fe0
Travis Vachon <travis.vachon@gmail.com>
2020-05-27 17:08:53 -0700

    make "apply" button react to applying and reduce duplicate applications

--- src/components/ApplyToJoinCultButton.js
+++ src/components/ApplyToJoinCultButton.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useState }from 'react'
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 export default function ApplyToJoinCultButton({webId, passport, cult}){
+  const [applied, setApplied] = useState(false)
   const apply = async () => {
     if (!passport.isFollowing(cult)) {
       passport.addFollowing(cult.asRef())
       await passport.save()
     }
     await cult.applyToJoin(webId)
+    setApplied(true)
   }
   return (
-    <Button onClick={apply}>
+    <>
       {cult && passport && (
         <>
-          {!passport.isFollowing(cult) && "Follow and "}
-          Apply to Join
+          {applied ? (
+            <Typography variant="body1">
+            The Cult Leader has been notified of your desire to swear fealty.
+            </Typography>
+          ) : (
+            <Button onClick={apply}>
+              {!passport.isFollowing(cult) && "Follow and "}
+              Apply to Join
+            </Button>
+          )}
         </>
       )}
-    </Button>
+    </>
   )
 }
